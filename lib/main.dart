61d91e77f2228ea97f99ad83d98db9fc2f10ff7f
Rifqi Eka Hardianto <ekarifqi1@gmail.com>
2022-01-04 20:40:48 +0700

    add face id permission for iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:local_auth/local_auth.dart';
 
@@ -7,53 +5,80 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final LocalAuthentication auth = LocalAuthentication();
-  bool isAuthenticated = false;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Biometric Authentication'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              isAuthenticated ? Text('Pesan rahasia') : SizedBox(),
-              ElevatedButton(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('Authenticate'),
-                    Icon(Icons.fingerprint),
-                  ],
-                ),
-                onPressed: () async {
-                  try {
-                    final result =
-                        await auth.authenticate(localizedReason: 'Reason');
-                    print(result);
-                    setState(() {
-                      isAuthenticated = result;
-                    });
-                  } catch (e) {
-                    setState(() {
-                      isAuthenticated = false;
-                    });
-                  }
-                },
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final LocalAuthentication localAuth = LocalAuthentication();
+  bool isAuthenticated = false;
+  String isSupported = 'tidak support';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkIsDeviceSupported();
+  }
+
+  checkIsDeviceSupported() async {
+    isSupported =
+        await localAuth.isDeviceSupported() ? 'support' : 'tidak support';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isSupported,
+            ),
+            isAuthenticated
+                ? Text(
+                    'Pesan Rahasia',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  )
+                : SizedBox(),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final bool result = await localAuth.authenticate(
+                  localizedReason: 'Untuk menampilkan pesan rahasia',
+                );
+
+                setState(() {
+                  isAuthenticated = result;
+                });
+
+                print(result);
+              },
+              child: Text(
+                'Biometric Auth',
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
